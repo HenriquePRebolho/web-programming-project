dd309833923257a7d3a18ec3dc6e3ba9063d00c2
henrique <henrique.rebolho@gmail.com>
2026-05-13 11:21:28 +0200

    Register page without reloading page (still need to configure db)

--- src/php/register.php
+++ src/php/register.php
@@ -1,51 +1,52 @@
 <?php
-
+    
     // php email: https://www.php.net/manual/en/function.mail.php
 
-    // Extract email
-    $email = $_POST['email'];
+    if (isset($_GET['email'])) {
+
+        // Extract email
+        $email = $_GET["email"];
+
+        // TODO: create user on database
+
+        // TODO: inform if user already exists with that email
 
 
-    // Analyze email
-    // ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
+        // TODO: Analyze email
+        // ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$
 
 
-    // Define password
-    // TODO: random or not?
-    $password = "Password123";
+        // Define subject // TODO: change Webshop
+        $subject = "Webshop - Login details";
+
+        // Define password
+        // TODO: random or not?
+        $password = "Password123";
 
 
-    // Caution: (Windows only) When PHP is talking to a SMTP server directly, 
-    // if a full stop is found on the start of a line, it is removed.
-    // To counter-act this, replace these occurrences with a double dot.
-    // TODO:
-    // $text = str_replace("\n.", "\n..", $text);
-    $message = "<h1>Welcome to Webshop!</h1><p>In order to activate your account use this temporary password:</p>".$password;
-
-    
-    // Define headers
-    // TODO: check if all of this is necessary
-    $headers[] = 'From: henrique.rebolho@gmail.com' . "\r\n" .
-    'Reply-To: henrique.rebolho@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    // To send HTML mail, the Content-type header must be set
-    $headers[] = 'MIME-Version: 1.0';
-    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-    // Additional headers
-    $headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
-    $headers[] = 'From: Birthday Reminder <birthday@example.com>';
-    $headers[] = 'Cc: birthdayarchive@example.com';
-    $headers[] = 'Bcc: birthdaycheck@example.com';
+        // Define html message // TODO: change "Webshop"
+        $message = "<h1>Welcome to Webshop!</h1>
+                    <p>In order to activate your account use this temporary password:</p>"
+                    .$password;
+        
+        // Define headers: To send HTML mail, the Content-type header must be set
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 
-    // Send email
-    mail(
-        to: $email,
-        subject: "Webshop - Login details",
-        message: $message,
-        additional_headers: implode("\r\n", $headers)
-    );
-
-    // TODO: add callback to register_page.php to inform email has been sent
-
+        // Send email
+        $success = mail(
+            to: $email,
+            subject: $subject,
+            message: $message,
+            additional_headers:  implode("\r\n", $headers)
+        );
+        if($success) {
+            echo("Confirmation email sent successfully.");
+        } else {
+            echo($errorMessage = error_get_last()['message']);
+        }
+    } else {
+        echo("Email is not set");
+    }
 ?>
--- src/register_page.php
+++ src/register_page.php
@@ -5,15 +5,43 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="HenriqueRebolloPadovani">
         <title>Register</title>
+
+        <!-- JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+        <script type="text/javascript">
+            var requestObj = new XMLHttpRequest();
+
+            function showEmailedConfirmed() {
+                if (requestObj.readyState == 4) {
+                    var serverAnswer = requestObj.responseText;
+                    var displayConfirmation = document.getElementById("sent");
+
+                    if (displayConfirmation != null) {
+                        displayConfirmation.innerHTML = serverAnswer;
+                    }
+                }
+            }
+
+        function confirmEmail() {
+            var email = document.getElementById("email").value;
+            if (email != "" && requestObj) {
+                requestObj.onreadystatechange = showEmailedConfirmed;
+                requestObj.open("get", "php/register.php?email="+email, true) // method, url, asyncrhonous
+                requestObj.send(null);
+            }
+            return false;
+        }
+        </script>
     </head>
 
     <body>
-        <form method="POST" action="php/register.php">
+        <form id="register_form" name="register_form">
             <label for="email">Email:</label>
-            <input type="email" id="email" placeholder="name@email.com" minlength="6">
-            <input type="submit" value="Register" onclick="confirmEmail()">
+            <input type="email" name="email" id="email" placeholder="name@email.com" minlength="6">
+            <input type="button" value="Register" onclick="confirmEmail()">
         </form>
-
+    
         <div id="sent"></div>
     </body>
 </html>
